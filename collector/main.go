8b967833558bbab777fe1d0bd5ce23490d988f09
Kazi Manzur Rashid <kazimanzurrashid@gmail.com>
2021-01-30 05:04:48 +0600

    added: implemented lambda handler for collector.

---
+++ collector/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-xray-sdk-go/xray"
+
+	"github.com/kazimanzurrashid/aws-scheduler-go/collector/storage"
+)
+
+var database storage.Storage
+
+func handler(ctx context.Context) error {
+	return database.Delete(ctx)
+}
+
+func init() {
+	ses := session.Must(session.NewSession())
+
+	ddbc := dynamodb.New(ses)
+	xray.AWS(ddbc.Client)
+
+	database = storage.NewDatabase(ddbc)
+}
+
+func main() {
+	lambda.Start(handler)
+}
