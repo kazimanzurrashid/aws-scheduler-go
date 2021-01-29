43fab44a3aad2f87bb245ff474d11eaddcde4f49
Kazi Manzur Rashid <kazimanzurrashid@gmail.com>
2021-01-30 02:10:28 +0600

    added: implemented lambda handler for graphql.

---
+++ graphql/main.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/graphql-go/graphql"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-xray-sdk-go/xray"
+
+	"github.com/kazimanzurrashid/aws-scheduler-go/graphql/api"
+	"github.com/kazimanzurrashid/aws-scheduler-go/graphql/storage"
+)
+
+type request struct {
+	Query         string                 `json:"query"`
+	OperationName string                 `json:"operationName"`
+	Variables     map[string]interface{} `json:"variables"`
+}
+
+var schema graphql.Schema
+
+var headers = map[string]string{
+	"Content-Type": "application/json;charset=utf-8",
+}
+
+type (
+	marshal   func(interface{}) ([]byte, error)
+	unmarshal func([]byte, interface{}) error
+)
+
+func jsonMarshal(v interface{}) ([]byte, error) {
+	return json.Marshal(v)
+}
+
+func jsonUnmarshal(data []byte, v interface{}) error {
+	return json.Unmarshal(data, v)
+}
+
+var (
+	marshalStruct   marshal   = jsonMarshal
+	unmarshalStruct unmarshal = jsonUnmarshal
+)
+
+func status(code int, err error) (events.APIGatewayV2HTTPResponse, error) {
+	buff, _ := marshalStruct(struct {
+		Result    string `json:"result"`
+		Timestamp string `json:"timestamp"`
+	}{
+		Result:    http.StatusText(code),
+		Timestamp: time.Now().Format(time.RFC3339),
+	})
+
+	res := events.APIGatewayV2HTTPResponse{
+		StatusCode: code,
+		Headers:    headers,
+		Body:       string(buff),
+	}
+
+	return res, err
+}
+
+func handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
+	body := strings.TrimSpace(req.Body)
+
+	if body == "" {
+		return status(http.StatusBadRequest, nil)
+	}
+
+	var ret interface{}
+
+	if strings.HasPrefix(body, "{") && strings.HasSuffix(body, "}") {
+		var payload request
+
+		if err := unmarshalStruct([]byte(body), &payload); err != nil {
+			return status(http.StatusInternalServerError, err)
+		}
+
+		ret = graphql.Do(graphql.Params{
+			Context:        ctx,
+			Schema:         schema,
+			RequestString:  payload.Query,
+			OperationName:  payload.OperationName,
+			VariableValues: payload.Variables,
+		})
+	} else if strings.HasPrefix(body, "[") && strings.HasSuffix(body, "]") {
+		var payloads []request
+
+		if err := unmarshalStruct([]byte(body), &payloads); err != nil {
+			return status(http.StatusInternalServerError, err)
+		}
+
+		rets := make([]*graphql.Result, len(payloads))
+		var wg sync.WaitGroup
+
+		for i, p := range payloads {
+			go func(payload request, index int) {
+				defer wg.Done()
+
+				out := graphql.Do(graphql.Params{
+					Context:        ctx,
+					Schema:         schema,
+					RequestString:  payload.Query,
+					OperationName:  payload.OperationName,
+					VariableValues: payload.Variables,
+				})
+
+				rets[index] = out
+			}(p, i)
+			wg.Add(1)
+		}
+
+		wg.Wait()
+
+		ret = rets
+	} else {
+		return status(http.StatusBadRequest, nil)
+	}
+
+	buff, err := marshalStruct(ret)
+
+	if err != nil {
+		return status(http.StatusInternalServerError, err)
+	}
+
+	res := events.APIGatewayV2HTTPResponse{
+		StatusCode: http.StatusOK,
+		Headers:    headers,
+		Body:       string(buff),
+	}
+
+	return res, nil
+}
+
+func init() {
+	ses := session.Must(session.NewSession())
+
+	ddbc := dynamodb.New(ses)
+	xray.AWS(ddbc.Client)
+
+	database := storage.NewDatabase(ddbc)
+
+	f := api.NewFactory(database)
+	s, err := f.Schema()
+
+	if err != nil {
+		log.Fatalf("schema create error: %v", err)
+		return
+	}
+
+	schema = s
+}
+
+func main() {
+	lambda.Start(handler)
+}
---
+++ graphql/main_test.go
@@ -0,0 +1,270 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/kazimanzurrashid/aws-scheduler-go/graphql/api"
+	"github.com/kazimanzurrashid/aws-scheduler-go/graphql/storage"
+	"github.com/stretchr/testify/assert"
+)
+
+type fakeStorage struct {
+	storage.Storage
+}
+
+//goland:noinspection GoUnusedParameter
+func (srv *fakeStorage) Get(ctx context.Context, id string) (*storage.Schedule, error) {
+	return &storage.Schedule{
+		ID:     "1234567890",
+		DueAt:  time.Now(),
+		URL:    "https://foo.bar/do",
+		Method: "POST",
+		Headers: map[string]string{
+			"accept": "application/json",
+		},
+		Body: "{ \"foo\": \"bar\" }",
+	}, nil
+}
+
+func Test_handler_Single_Request_Success(t *testing.T) {
+	realSchema := schema
+	f := api.NewFactory(&fakeStorage{})
+	s, err := f.Schema()
+	if err != nil {
+		t.Error(err)
+	}
+	schema = s
+
+	bodyStruct := request{
+		Query: "query Get($id: ID!) { get(id: $id) { id url method } }",
+		Variables: map[string]interface{}{
+			"id": "01234567890",
+		},
+	}
+
+	bodyBuff, err := json.Marshal(bodyStruct)
+	if err != nil {
+		t.Error(err)
+	}
+
+	req := events.APIGatewayV2HTTPRequest{
+		Body: string(bodyBuff),
+	}
+
+	res, err := handler(context.TODO(), req)
+
+	assert.NotNil(t, res)
+	//goland:noinspection GoNilness
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+	assert.Nil(t, err)
+
+	schema = realSchema
+}
+
+func Test_handler_Single_Request_Fail_Unmarshal_Error(t *testing.T) {
+	realSchema := schema
+	f := api.NewFactory(&fakeStorage{})
+	s, err := f.Schema()
+	if err != nil {
+		t.Error(err)
+	}
+	schema = s
+
+	realUnmarshal := unmarshalStruct
+
+	unmarshalStruct = func(bytes []byte, i interface{}) error {
+		return fmt.Errorf("unmarshal error")
+	}
+
+	bodyStruct := request{
+		Query: "query Get($id: ID!) { get(id: $id) { id url method } }",
+		Variables: map[string]interface{}{
+			"id": "01234567890",
+		},
+	}
+
+	bodyBuff, err := json.Marshal(bodyStruct)
+	if err != nil {
+		t.Error(err)
+	}
+
+	req := events.APIGatewayV2HTTPRequest{
+		Body: string(bodyBuff),
+	}
+
+	res, err := handler(context.TODO(), req)
+
+	assert.NotNil(t, res)
+	//goland:noinspection GoNilness
+	assert.Equal(t, http.StatusInternalServerError, res.StatusCode)
+	assert.NotNil(t, err)
+
+	unmarshalStruct = realUnmarshal
+	schema = realSchema
+}
+
+func Test_handler_Multi_Request_Success(t *testing.T) {
+	realSchema := schema
+	f := api.NewFactory(&fakeStorage{})
+	s, err := f.Schema()
+	if err != nil {
+		t.Error(err)
+	}
+	schema = s
+
+	bodyStruct := []request{
+		{
+			Query: "query Get($id: ID!) { get(id: $id) { id url method } }",
+			Variables: map[string]interface{}{
+				"id": "01234567890",
+			},
+		},
+		{
+			Query: "query Get($id: ID!) { get(id: $id) { id url method } }",
+			Variables: map[string]interface{}{
+				"id": "01234567890",
+			},
+		},
+	}
+
+	bodyBuff, err := json.Marshal(bodyStruct)
+	if err != nil {
+		t.Error(err)
+	}
+
+	req := events.APIGatewayV2HTTPRequest{
+		Body: string(bodyBuff),
+	}
+
+	res, err := handler(context.TODO(), req)
+
+	assert.NotNil(t, res)
+	//goland:noinspection GoNilness
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+	assert.Nil(t, err)
+
+	schema = realSchema
+}
+
+func Test_handler_Multi_Request_Fail_Unmarshal_Error(t *testing.T) {
+	realSchema := schema
+	f := api.NewFactory(&fakeStorage{})
+	s, err := f.Schema()
+	if err != nil {
+		t.Error(err)
+	}
+	schema = s
+
+	realUnmarshal := unmarshalStruct
+
+	unmarshalStruct = func(bytes []byte, i interface{}) error {
+		return fmt.Errorf("unmarshal error")
+	}
+
+	bodyStruct := []request{
+		{
+			Query: "query Get($id: ID!) { get(id: $id) { id url method } }",
+			Variables: map[string]interface{}{
+				"id": "01234567890",
+			},
+		},
+		{
+			Query: "query Get($id: ID!) { get(id: $id) { id url method } }",
+			Variables: map[string]interface{}{
+				"id": "01234567890",
+			},
+		},
+	}
+
+	bodyBuff, err := json.Marshal(bodyStruct)
+	if err != nil {
+		t.Error(err)
+	}
+
+	req := events.APIGatewayV2HTTPRequest{
+		Body: string(bodyBuff),
+	}
+
+	res, err := handler(context.TODO(), req)
+
+	assert.NotNil(t, res)
+	//goland:noinspection GoNilness
+	assert.Equal(t, http.StatusInternalServerError, res.StatusCode)
+	assert.NotNil(t, err)
+
+	unmarshalStruct = realUnmarshal
+	schema = realSchema
+}
+
+func Test_handler_Fail_Empty_Body(t *testing.T) {
+	req := events.APIGatewayV2HTTPRequest{}
+
+	res, err := handler(context.TODO(), req)
+
+	assert.NotNil(t, res)
+	//goland:noinspection GoNilness
+	assert.Equal(t, http.StatusBadRequest, res.StatusCode)
+	assert.Nil(t, err)
+
+}
+
+func Test_handler_Fail_Invalid_Body(t *testing.T) {
+	req := events.APIGatewayV2HTTPRequest{
+		Body: "foo=bar",
+	}
+
+	res, err := handler(context.TODO(), req)
+
+	assert.NotNil(t, res)
+	//goland:noinspection GoNilness
+	assert.Equal(t, http.StatusBadRequest, res.StatusCode)
+	assert.Nil(t, err)
+}
+
+func Test_handler_Fail_Marshal_Error(t *testing.T) {
+	realSchema := schema
+	f := api.NewFactory(&fakeStorage{})
+	s, err := f.Schema()
+	if err != nil {
+		t.Error(err)
+	}
+	schema = s
+
+	realMarshal := marshalStruct
+
+	marshalStruct = func(i interface{}) ([]byte, error) {
+		return nil, fmt.Errorf("marshal error")
+	}
+
+	bodyStruct := request{
+		Query: "query Get($id: ID!) { get(id: $id) { id url method } }",
+		Variables: map[string]interface{}{
+			"id": "01234567890",
+		},
+	}
+
+	bodyBuff, err := json.Marshal(bodyStruct)
+	if err != nil {
+		t.Error(err)
+	}
+
+	req := events.APIGatewayV2HTTPRequest{
+		Body: string(bodyBuff),
+	}
+
+	res, err := handler(context.TODO(), req)
+
+	assert.NotNil(t, res)
+	//goland:noinspection GoNilness
+	assert.Equal(t, http.StatusInternalServerError, res.StatusCode)
+	assert.NotNil(t, err)
+
+	marshalStruct = realMarshal
+	schema = realSchema
+}
