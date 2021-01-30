179554021c796a1807dc17585a7bd26e7ab144aa
Kazi Manzur Rashid <kazimanzurrashid@gmail.com>
2021-01-30 06:05:33 +0600

    chore: updated deploymnet script.

--- stack/deploy.sh
+++ stack/deploy.sh
@@ -3,17 +3,15 @@
 cd ../graphql || exit
 mkdir -p dist
 CGO_ENABLED=0 go build -o dist/main
-cd dist && zip -r -9 scheduler-graphql-v1.zip ./* && rm main
 
-cd ../../collector || exit
+cd ../collector || exit
 mkdir -p dist
 CGO_ENABLED=0 go build -o dist/main
-cd dist && zip -r -9 scheduler-collector-v1.zip ./* && rm main
 
-cd ../../worker || exit
+cd ../worker || exit
 mkdir -p dist
 CGO_ENABLED=0 go build -o dist/main
-cd dist && zip -r -9 scheduler-worker-v1.zip ./* && rm main
 
-cd ../../stack || exit
+cd ../stack || exit
+cdk bootstrap
 cdk deploy --require-approval never
