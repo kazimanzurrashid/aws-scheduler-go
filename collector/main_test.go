87581d921b0b064ea6e5b844be790c427b956699
Kazi Manzur Rashid <kazimanzurrashid@gmail.com>
2021-01-30 06:25:44 +0600

    added: missing test collector lambda handler.

---
+++ collector/main_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"testing"
+
+	"github.com/kazimanzurrashid/aws-scheduler-go/collector/storage"
+	"github.com/stretchr/testify/assert"
+)
+
+type fakeStorage struct {
+	storage.Storage
+
+	Called bool
+}
+
+//goland:noinspection GoUnusedParameter
+func (srv *fakeStorage) Delete(ctx context.Context) error  {
+	srv.Called = true
+	return nil
+}
+
+func Test_handler_Success(t *testing.T) {
+	fake := fakeStorage{}
+	database = &fake
+
+	_ = handler(context.TODO())
+
+	assert.True(t, fake.Called)
+}
