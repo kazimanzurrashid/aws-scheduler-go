297f6515cdad28af32d8c733b0b8e454bd25d309
Kazi Manzur Rashid <kazimanzurrashid@gmail.com>
2021-02-02 18:01:08 +0600

    chore: 'added build support for windows'

--- stack/deploy.sh
+++ stack/deploy.sh
@@ -2,15 +2,15 @@
 
 cd ../graphql || exit
 mkdir -p dist
-CGO_ENABLED=0 go build -o dist/main
+CGO_ENABLED=0 GOOS=linux go build -o dist/main
 
 cd ../collector || exit
 mkdir -p dist
-CGO_ENABLED=0 go build -o dist/main
+CGO_ENABLED=0 GOOS=linux go build -o dist/main
 
 cd ../worker || exit
 mkdir -p dist
-CGO_ENABLED=0 go build -o dist/main
+CGO_ENABLED=0 GOOS=linux go build -o dist/main
 
 cd ../stack || exit
 cdk bootstrap
