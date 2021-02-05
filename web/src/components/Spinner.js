02e5ed6b41ac5a799daebe0277d1a1deea6902ec
Kazi Manzur Rashid <kazimanzurrashid@gmail.com>
2021-02-05 22:24:51 +0600

    chore(client): added web in dependabot.

---
+++ web/src/components/Spinner.js
@@ -0,0 +1,27 @@
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const Style = makeStyles(() => {
+  return {
+    root: {
+      left: '50%',
+      position: 'absolute',
+      textAlign: 'center',
+      top: '50%',
+      transform: 'translate(-50%, -50%)',
+      verticalAlign: 'middle',
+    },
+  };
+});
+
+const Spinner = props => {
+  const style = Style();
+
+  return (
+    <div className={style.root}>
+      <CircularProgress {...props} />
+    </div>
+  );
+};
+
+export default Spinner;
