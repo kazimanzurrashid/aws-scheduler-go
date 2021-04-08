a610200613baec982ee166b6d283754c3a45ede6
Kazi Manzur Rashid <kazimanzurrashid@gmail.com>
2021-04-09 03:24:06 +0600

    style: playground

--- graphql/main.go
+++ graphql/main.go
@@ -99,7 +99,7 @@ func handler(
 		return events.APIGatewayV2HTTPResponse{
 			StatusCode: http.StatusOK,
 			Headers: map[string]string{
-				"Cache-Control": "public,max-age=31536000,immutable",
+				"Cache-Control": "private,max-age=31536000,immutable",
 				"Content-Type":  "text/html;charset=utf-8",
 			},
 			Body: buffer.String(),
