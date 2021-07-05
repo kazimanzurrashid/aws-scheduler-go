dff386ddd2435830afa4899f701d79ab230d7bf0
Kazi Manzur Rashid <kazimanzurrashid@gmail.com>
2021-07-05 11:40:27 +0600

    refactor: addressed deep source warning sorted import

--- web/src/pages/Details.js
+++ web/src/pages/Details.js
@@ -5,23 +5,23 @@ import { Link as RouterLink, useHistory, useParams } from 'react-router-dom';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
-import MuiLink from '@material-ui/core/Link';
-import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import Tooltip from '@material-ui/core/Tooltip';
-import IconButton from '@material-ui/core/IconButton';
 import CloneIcon from '@material-ui/icons/Launch';
-import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import IconButton from '@material-ui/core/IconButton';
+import MuiLink from '@material-ui/core/Link';
+import Tooltip from '@material-ui/core/Tooltip';
+import Typography from '@material-ui/core/Typography';
 
 import Api from '../api';
-import Spinner from '../components/Spinner';
 import CopyToClipboardButton from '../components/CopyToClipboardButton';
+import Spinner from '../components/Spinner';
 
 const Styles = makeStyles(theme => ({
   breadcrumbs: {
