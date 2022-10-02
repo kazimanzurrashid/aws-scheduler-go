bdfa8e1931cc7317cf0e301e6b979becdc3374cf
Kazi Manzur Rashid <kazimanzurrashid@gmail.com>
2022-10-02 16:08:08 +0600

    chore: upgrade web npm dependencies

--- web/src/index.js
+++ web/src/index.js
@@ -4,4 +4,4 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 
 // noinspection JSCheckFunctionSignatures
-createRoot(document.getElementById('root')).render(<App/>);
+createRoot(document.getElementById('root')).render(<App />);
