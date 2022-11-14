3262dbd8ae15d2b526797817198ccf2da206912a
Kazi Manzur Rashid <kazimanzurrashid@gmail.com>
2022-11-14 08:13:32 +0600

    chore: downgrade to react 17

--- web/src/index.js
+++ web/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
 import App from './App';
 
-// noinspection JSCheckFunctionSignatures
-createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.render(<App />, document.getElementById('root'));
